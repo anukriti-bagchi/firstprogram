9036d8d75f22271483fc5e4a49956c211ff9145e
Anukriti Bagchi <91126891+anukriti-bagchi@users.noreply.github.com>
2023-03-26 16:14:02 +0000

    added new text

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
 print("hello world, welcome to 100 days of coding by code with harry")
+print("This course by Harry that is provided for free on youtube needs a student's consistency to become job ready")
