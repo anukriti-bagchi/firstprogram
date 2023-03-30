94c26d563a808ee83c5b62f70646bf29e335a4b7
Anukriti Bagchi <91126891+anukriti-bagchi@users.noreply.github.com>
2023-03-30 08:12:00 +0000

    .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Hello World, welcome to 100 days of coding by code with Harry")
 print("This course by Harry that is provided for free on youtube needs a student's consistency to become job ready")
 print("So far people have come up with good reviews on this course which made me try out and get started with the course on Python")
-print()
+print("So far people have come up with good reviews on this course which made me try out and get started with the course on Python")
